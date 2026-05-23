1d130dd279e543a3ccf913461c450862848d9053
foxie <fabianverhagen2011@gmail.com>
2026-05-23 18:00:13 +0200

    Producten en categorien

--- web/KE03_INTDEV_SE_1_Base/Pages/Producten.cshtml.cs
+++ web/KE03_INTDEV_SE_1_Base/Pages/Producten.cshtml.cs
@@ -1,5 +1,23 @@
 using DataAccessLayer.Interfaces;
 using DataAccessLayer.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace KE03_INTDEV_SE_1_Base.Pages
+{
+    public class ProductenModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+
+        public IList<Product> Products { get; set; } = new List<Product>();
+
+        public ProductenModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void OnGet()
+        {
+            Products = _productRepository.GetAllProducts().ToList();
+        }
+    }
+}
