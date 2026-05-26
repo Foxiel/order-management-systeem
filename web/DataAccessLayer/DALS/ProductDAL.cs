17f7aa169108925bbb9762b9714c118f8e0ebb16
foxie <fabianverhagen2011@gmail.com>
2026-05-26 10:45:12 +0200

    Product pagina

--- web/DataAccessLayer/DALS/ProductDAL.cs
+++ web/DataAccessLayer/DALS/ProductDAL.cs
@@ -1,4 +1,5 @@
 ﻿//Gemaakt door Tristan
+
 using DataAccessLayer.Models;
 using Microsoft.Data.SqlClient;
 
@@ -57,6 +58,61 @@ namespace DataAccessLayer.DAL
             }
 
             return products;
+        }
+
+        // Gemaakt door Fabian
+        public Product GetProductByEAN(string ean)
+        {
+            Product product = new Product();
+
+            using SqlConnection conn = GetConnection();
+
+            string query = query = @"
+        SELECT 
+            p.ProductEAN,
+            p.ProductName,
+            p.ProductDescription,
+            p.ProductSpecification,
+            p.ProductPrice,
+            p.ProductStock,
+            p.ProductWeightKg,
+            p.ProductWarrantyMonths,
+            p.ProductReleaseDate,
+            p.ManufacturerId,
+            p.SubcategoryId,
+            pi.ImageUrl
+        FROM Product p
+        LEFT JOIN ProductImage pi 
+            ON p.ProductEAN = pi.ProductEAN 
+            AND pi.IsMainImage = 1
+        WHERE p.ProductEAN = @ProductEAN";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ProductEAN", ean);
+
+            conn.Open();
+
+            using SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                product = new Product
+                {
+                    ProductEAN = reader["ProductEAN"].ToString()!,
+                    ProductName = reader["ProductName"].ToString()!,
+                    ProductDescription = reader["ProductDescription"].ToString(),
+                    ProductSpecification = reader["ProductSpecification"].ToString(),
+                    ProductPrice = Convert.ToDecimal(reader["ProductPrice"]),
+                    ProductStock = Convert.ToInt32(reader["ProductStock"]),
+                    ProductWeightKg = Convert.ToDecimal(reader["ProductWeightKg"]),
+                    ProductWarrantyMonths = Convert.ToInt32(reader["ProductWarrantyMonths"]),
+                    ProductReleaseDate = Convert.ToDateTime(reader["ProductReleaseDate"]),
+                    ManufacturerId = reader["ManufacturerId"].ToString()!,
+                    SubcategoryId = reader["SubcategoryId"].ToString()!,
+                    ImageUrl = reader["ImageUrl"] == DBNull.Value ? null : reader["ImageUrl"].ToString()
+                };
+            }
+
+            return product;
         }
     }
 }
--- web/KE03_INTDEV_SE_1_Base/Pages/Product.cshtml.cs
+++ web/KE03_INTDEV_SE_1_Base/Pages/Product.cshtml.cs
@@ -1,11 +1,29 @@
+using DataAccessLayer.DAL;
+using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace KE03_INTDEV_SE_1_Base.Pages;
-
-public class Product : PageModel
+namespace KE03_INTDEV_SE_1_Base.Pages
 {
-    public void OnGet()
+    public class ProductModel : PageModel
     {
-        
+        // Gemaakt door Fabian
+        public Product? Product { get; set; }
+
+        // id komt uit querystring: /Product?id=ACT-003
+        public void OnGet([FromQuery] string? id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                Product = null;
+                return;
+            }
+
+            ProductDAL productDAL = new ProductDAL();
+            Product = productDAL.GetProductByEAN(id);
+
+            // Als jouw DAL een 'default' Product teruggeeft bij niet-gevonden,
+            // kun je hier extra checken: if (Product != null && string.IsNullOrEmpty(Product.ProductEAN)) Product = null;
+        }
     }
 }
